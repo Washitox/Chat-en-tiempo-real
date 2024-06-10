f3d74bcd4d9cfbe1cd1a9138f183064836f5778e
Washington <washovilla78@gmail.com>
2024-06-10 15:36:03 -0500

    Finalización del proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAGZmu7eeUJuja76N0mnTVNIrNs6WrxeDM",
+    authDomain: "chat-app-c0aff.firebaseapp.com",
+    projectId: "chat-app-c0aff",
+    storageBucket: "chat-app-c0aff.appspot.com",
+    messagingSenderId: "538538969818",
+    appId: "1:538538969818:web:067d2296402804c20233e3",
+    measurementId: "G-N5VPMLEJ3J"
+    }
 };
